1693d4c6a81b77ff19c62bd8bd7f9927638f38ce
fradeet <41571697+fradeet@users.noreply.github.com>
2026-06-17 16:16:36 +0800

    Create goatcounter.js

---
+++ goatcounter.js
@@ -0,0 +1,11 @@
+(function () {
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = "https://gc.zgo.at/count.js";
+  script.setAttribute(
+    "data-goatcounter",
+    "https://jeekmind-help.goatcounter.com/count"
+  );
+
+  document.head.appendChild(script);
+})();
